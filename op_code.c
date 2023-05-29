525db2ae7fc4d6f1c3883789b2b9ade6ae0f0871
Belove <beloveolusola@gmail.com>
2023-05-29 01:56:39 +0100

    Pall

--- op_code.c
+++ op_code.c
@@ -57,3 +57,27 @@ void push_queue(stack_t **stack, unsigned int p)
 	new_node->prev = NULL;
 	*stack = new_node;
 }
+
+/**
+ * pall - Prints all the values on the stack,
+ * starting from the top of the stack.
+ * @stack: The head of the stack
+ * @line_number: The line on which the error occurred
+ *
+ * Return: Nothing
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current = NULL;
+	(void) line_number;
+
+	if (*stack)
+	{
+		current = *stack;
+		while (current != NULL)
+		{
+			printf("%d\n", current->n);
+			current = current->next;
+		}
+	}
+}
